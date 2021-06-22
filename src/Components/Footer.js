049ae5abfa7b10d6b1528b6a9a0e4f67300bfa58
Rishi Jha <47343875+rishiiee@users.noreply.github.com>
2021-06-22 12:21:59 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -20,7 +20,7 @@ class Footer extends Component {
 
            <ul className="copyright">
               <li>&copy;</li>
-              <li>Design by <a title="Styleshout">Styleshout</a></li>
+              <li>Design by <a title="Styleshout">Rishi</a></li>
            </ul>
 
         </div>
